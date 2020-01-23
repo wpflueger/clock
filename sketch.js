31877115ea65e86cd9680d1ad1d2120f000be590
wpflueger <will.pflueger@gmail.com>
2020-01-23 15:11:20 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,8 +6,7 @@ function draw() {
   background("black");
   noStroke();
   //frameRate(1);
-  fill("#fcd14d");
-  ellipse(0, 0, 300);
+
   randomSeed(123);
   for (let i = 0; i < 100; i++) {
     x_pos = random(0, 800);
@@ -15,14 +14,20 @@ function draw() {
     drawStars(x_pos, y_pos);
   }
 
+  //Draw Sun
+  fill("#fcd14d");
+  ellipse(0, 0, 300);
+
   //Draw Planets
+
   //Hour
   fill("#6b93d6");
   ellipse(
-    600 * cos(map(hour(), 0, 60, PI / 2, 0)),
-    600 * sin(map(hour(), 0, 60, PI / 2, 0)),
+    600 * cos(map(hour(), 0, 24, PI / 2, 0)),
+    600 * sin(map(hour(), 0, 24, PI / 2, 0)),
     50
   );
+
   //Minute
   fill("#BBB7AB");
   ellipse(
@@ -30,6 +35,7 @@ function draw() {
     450 * sin(map(minute(), 0, 60, PI / 2, 0)),
     50
   );
+
   //Second Fixed
   fill("#97979F");
   ellipse(
